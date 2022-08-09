cd6acb722e649bf6ba93fab8ef8aeea355d52b19
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-08-08 22:39:51 -0300

    feat: adicionando componente Itens e Item

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,6 +4,8 @@ import Buscador from './Buscador';
 import { useState } from 'react';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
+import Itens from './Itens';
+
 
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
@@ -30,6 +32,7 @@ export default function Cardapio() {
                     <Filtros filtro={filtro} setFiltro={setFiltro}/>
                     <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
                 </div>
+                <Itens />
             </section>
         </main>
     )
