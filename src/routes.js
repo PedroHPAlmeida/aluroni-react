6e903441e66c72a4d23826ec8fdee7757e8d3484
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-08-28 14:05:19 -0300

    fix: exibindo 100% pagina NotFound

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function AppRouter() {
 	return (
-		<main>
+		<main className='container'>
 			<Router>
 				<Menu />
 				<Routes>
