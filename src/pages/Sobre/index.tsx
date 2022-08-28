cf0df0c5ae6fe3c522c0c1902fdd259b0647222c
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-08-28 13:32:47 -0300

    feat: adicionando pagina de Sobre

---
+++ src/pages/Sobre/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Sobre.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+
+
+export default function Sobre() {
+	return (
+		<section>
+            Sobre
+		</section>
+	);
+}
